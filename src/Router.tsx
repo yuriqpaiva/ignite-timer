315c4dcf9c69bc630070637002638c291f95d3f7
Yuri <yuriqpaiva@gmail.com>
2023-04-05 20:11:45 -0300

    Add react-router-dom router structure

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,22 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { History } from './pages/History'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/history',
-    element: <History />,
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/history',
+        element: <History />,
+      },
+    ],
   },
 ])
 
